653dcf086c54e6640162ff0d43c478aeebfff4e2
exc- <deadbender27@gmail.com>
2019-05-19 16:49:35 +0600

    added task4

---
+++ src/LinkedHashTable.h
@@ -0,0 +1,80 @@
+#pragma once
+
+struct NoElemException {
+};
+
+
+struct Element {
+    int data;
+    int key;
+    Element *next;
+
+    Element(int data, int key, Element *next = nullptr);
+};
+
+class HashList {
+private:
+    Element *first;
+    Element *last;
+public:
+    friend class Iterator;
+
+    HashList();
+
+    ~HashList();
+
+    void addElem(int elem, int key);
+
+    void delElem(int elem);
+
+    void doEmpty();
+};
+
+class LinkedHashTable {
+private:
+    Element **hashTable;
+    HashList *list;
+    int size;
+    int countElem;
+public:
+    friend class Iterator;
+
+    LinkedHashTable();
+
+    explicit LinkedHashTable(int size);
+
+    void addElem(int elem, int key);
+
+    void removeElem(int key);
+
+    int getElem(int key);
+
+    void doEmpty();
+
+    bool isEmpty();
+
+    void printElem();
+
+    ~LinkedHashTable();
+};
+
+
+class Iterator {
+private:
+    Element *now;
+    LinkedHashTable *hashList;
+
+public:
+    explicit Iterator(LinkedHashTable *&hashTable);
+
+    ~Iterator();
+
+    bool hasNext();
+
+    void start();
+
+    void next();
+
+    int seeElem();
+};
+
---
+++ src/Main.cpp
@@ -0,0 +1,38 @@
+#include "LinkedHashTable.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    auto *oneHashTable = new LinkedHashTable();
+    oneHashTable->addElem(1, 0);
+    oneHashTable->addElem(2, 3);
+    oneHashTable->addElem(3, 2);
+    oneHashTable->addElem(4, 5);
+    oneHashTable->addElem(15, 7);
+    oneHashTable->addElem(5, 12);
+    oneHashTable->printElem();
+    Iterator iteratorOne(oneHashTable);
+    while (iteratorOne.hasNext()) {
+        cout << iteratorOne.seeElem() << endl;
+        iteratorOne.next();
+    }
+    cout << iteratorOne.seeElem() << endl;
+    cout << "-----------------------------" << endl;
+    oneHashTable->removeElem(3);
+    iteratorOne.start();
+    while (iteratorOne.hasNext()) {
+        cout << iteratorOne.seeElem() << endl;
+        iteratorOne.next();
+    }
+    cout << iteratorOne.seeElem() << endl;
+    cout << "-----------------------------" << endl;
+    cout << oneHashTable->getElem(0) << endl;
+    cout << oneHashTable->getElem(5) << endl;
+    cout << "isEmpty: " << oneHashTable->isEmpty() << endl;
+    cout << "doEmpty." << endl;
+    oneHashTable->doEmpty();
+    cout << "isEmpty: " << oneHashTable->isEmpty() << endl;
+    return 0;
+}
+
